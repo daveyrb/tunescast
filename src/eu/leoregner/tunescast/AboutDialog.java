5225d2b687a36ca139b7bc30441a9d57435c1ed5
Leopold Mathias Regner <leopold.mathias.regner@gmail.com>
2017-05-18 00:02:16 +0200

    adding note about icons

--- src/eu/leoregner/tunescast/AboutDialog.java
+++ src/eu/leoregner/tunescast/AboutDialog.java
@@ -11,12 +11,12 @@ public class AboutDialog extends JDialog
 		setAlwaysOnTop(true);
 		
 		final StringBuffer aboutText = new StringBuffer();
-		aboutText.append("<font face='Comic Sans MS'>");
 		aboutText.append("<h1>TunesCast</h1>");
 		aboutText.append("<p>&copy; Leopold Mathias Regner<br/>published under GNU General Public License</p>");
 		aboutText.append("<p>More information can be found on the website and the project's GitHub repository:");
 		aboutText.append("<ul><li>https://leoregner.eu/product/2</li>");
 		aboutText.append("<li>https://github.com/leoregner/tunescast</li></ul>");
+		aboutText.append("<p>Icons provided by EmojiOne.com and icons8.com</p>");
 		
 		final JEditorPane textPane = new JEditorPane();
 		textPane.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
@@ -27,7 +27,7 @@ public class AboutDialog extends JDialog
 		textPane.setFont(new java.awt.Font("Arial", java.awt.Font.PLAIN, 14));
 		setContentPane(textPane);
 		
-		final int width = 550, height = 230;
+		final int width = 550, height = 250;
 		setSize(width, height);
 		setLocation((getToolkit().getScreenSize().width - width) / 2, (getToolkit().getScreenSize().height - height) / 2);
 	}
